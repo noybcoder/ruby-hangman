d7924a8a04d693245a65b21d0796b7e4e63e81e0
Preston Lam <preslam911@gmail.com>
2024-06-30 13:58:39 -0600

    added new methods to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,21 +2,65 @@ require_relative 'player'
 require_relative 'computer'
 
 class Game
-  attr_accessor :player, :computer, :guess_display, :wrong_letters
+  attr_accessor :player, :computer, :remaining_chances, :guess_display, :wrong_letters
 
   def initialize
     @player = Player.new
     @computer = Computer.new
+    @remaining_chances = 10
     @guess_display = Array.new(computer.secret_word.length, '?')
     @wrong_letters = []
   end
 
-  def verify_guess
-    puts computer.secret_word
-    computer.secret_word.include?(player.make_guess)
+  def play
+    until win? || lose?
+      puts @remaining_chances
+      update_progress
+      puts guess_display.join(' ')
+      p @wrong_letters
+      if win?
+        puts 'You win!'
+      elsif lose?
+        puts 'You lose!'
+      end
+    end
+  end
+
+  def update_progress
+    guess = player.make_guess
+    answer = computer.secret_word
+
+    current_display = answer.chars.map {|char| char == guess ? guess : '?'}
+    if incorrect_guess?(current_display)
+      @remaining_chances -= 1
+      store_wrong_letters(guess)
+    else
+      update_display(current_display)
+    end
+  end
+
+  def win?
+    @guess_display.none?('?')
+  end
+
+  def lose?
+    @remaining_chances.zero? && !win?
+  end
+
+  def incorrect_guess?(current_display, symbol='?')
+    current_display.all?(symbol)
+  end
+
+  def store_wrong_letters(guess)
+    @wrong_letters << guess
+  end
+
+  def update_display(current_display)
+    @guess_display = current_display
   end
 
 end
 
 game = Game.new
-p game.verify_guess
+
+game.play
